4e4ea27817db5dd894f1f75aa2f17cb532d380ee
tengwangyang <65354151+twy2020@users.noreply.github.com>
2024-01-11 00:29:17 +0800

    Update test.py

--- Projects/讯飞星火大模型API调用python/test.py
+++ Projects/讯飞星火大模型API调用python/test.py
@@ -1,7 +1,7 @@
 import SparkApi
 
 # 以下密钥信息从控制台获取
-appid = "68003377"  # 填写控制台中获取的 APPID 信息
+appid = ""  # 填写控制台中获取的 APPID 信息
 api_secret = ""  # 填写控制台中获取的 APISecret 信息
 api_key = ""  # 填写控制台中获取的 APIKey 信息
 
